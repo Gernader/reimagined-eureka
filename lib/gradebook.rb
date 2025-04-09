c4212a0a7d399383d858f05b6a7b21bcb1dc668b
William Ross <bill_87_2@yahoo.com>
2025-04-09 11:38:10 -0500

    Add_course method

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,13 @@
 class Gradebook
-  
+  attr_reader :name, :courses
+
+  def initialize(name)
+    @name = name
+    @courses = []
+
+  end
+
+  def add_course(course)
+    @courses << course
+  end
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -5,21 +5,28 @@ require './lib/gradebook'
 
 RSpec.describe Gradebook do
   it 'can exist' do
-    gradebook = Gradebook.new("Snape"[("Potions", 12)])
+    gradebook = Gradebook.new("Snape")
 
     expect(gradebook).to be_an_instance_of(Gradebook)
+    
   end
 
   it 'can add courses' do
+    gradebook = Gradebook.new("Snape")
+    @course1 = Course.new("Potions", 12)
+    gradebook.add_course(@course1)
+
+    expect(gradebook.courses).to eq([@course1])
+
 
   end
 
-  it 'can list all students' do
+  xit 'can list all students' do
 
   end
 
-  it 'list students below threshold' do
-    
+  xit 'list students below threshold' do
+
   end
 
     
